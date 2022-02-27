3b6d7eadd2b4bcb04d57a09b9604867691577bc4
salveketan <salveketan6@gmail.com>
2022-02-27 19:43:24 +0530

    ketan

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pepperfry_Clone
-This is a cloned website of pepperfry.com
---
+++ checkoutNavbar.js
@@ -0,0 +1,8 @@
+let search_input = document.getElementById("search");
+search_input.addEventListener("click", () => {
+  search_input.placeholder = "What are you looking for?";
+  search_input.style.border = "none";
+});
+
+
+
