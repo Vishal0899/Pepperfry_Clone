0b03b0587a389510b0a65a284706830d27c659c2
Nishut Suman Mahunta <nishutsuman1998@gmail.com>
2022-02-27 19:47:25 +0530

    Index Files Added
    Nishut File Submitted for the landing page

---
+++ index_data.js
@@ -0,0 +1,85 @@
+let sec1_img= {
+    img1:'https://ii1.pepperfry.com/media/wysiwyg/banners/Promo_Web_2X_220222_et.jpg',    //size=888X432.86
+    img2:'https://ii2.pepperfry.com/media/wysiwyg/banners/HB02_Web_210222.jpg',
+    img3:'https://ii3.pepperfry.com/media/wysiwyg/banners/HB03_Web_210222.jpg',
+    img4:'https://ii1.pepperfry.com/media/wysiwyg/banners/HB04_Web_210222.jpg',
+    gif: 'https://ii2.pepperfry.com/media/wysiwyg/banners/Home-Fashion-Week_RHS.gif'  //size= 287.77X432.86
+}
+
+let sec2= {     //size= 288 X 215.78
+    img1: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Rooms_Section_Web_1__2x_17022022.jpg",
+    img2: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Rooms_Section_Web_2__2x_17022022.jpg",
+    img3: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Rooms_Section_Web_3__2x_17022022.jpg",
+    img4: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Rooms_Section_Web_4__2x_17022022.jpg",
+}
+
+let sec3= { //size= 387.7 X 417.67
+    img1= "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_1_2x_17022022-new.jpg",
+    img2= "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_2_2x_17022022-new.jpg",
+    // size= 187.84 X 202.94 or .53
+    img3= "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_3_2x_17022022.jpg",
+    img4= "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_5_2x_17022022-new.jpg",
+    img5= "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_4_2x_17022022.jpg",
+    img6= "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Trend_Section_WEB_6_2x_17022022.jpg",
+}
+
+let sec4= {   //size= 288 X 332.7
+    img1: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_1_2x_17022022.jpg",
+    img2: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_2_2x_17022022.jpg",
+    img3: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_3_2x_17022022-new.jpg",
+    img4: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_3_2x_17022022.jpg",
+    img4: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_5_2x_17022022.jpg",
+    img4: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Budget_Block_WEB_6_2x_17022022.jpg",
+}
+
+let sec5= {
+    img1: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Style_Block_Web_1_2x_17022022.jpg",
+    img2: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Style_Block_Web_2_2x_17022022.jpg",
+    img3: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Style_Block_Web_3_2x_17022022.jpg",
+}
+
+let sec6= {
+    img1: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_1_2x_17022022.jpg",
+    img2: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_2_2x_17022022.jpg",
+    img3: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_3_2x_17022022.jpg",
+    img4: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_4_2x_17022022.jpg",
+    img5: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_5_2x_17022022.jpg",
+    img6: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Collection_Block_WEB_6_2x_17022022.jpg",
+}
+
+let sec7= {
+    img1:"https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_1_2x_17022022.jpg",
+    img2:"https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_2_2x_17022022.jpg",
+    img3:"https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_3_2x_17022022.jpg",
+    img4:"https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_4_2x_17022022.jpg",
+    img5:"https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_5_2x_17022022.jpg",
+    img6:"https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Decor_Block_WEB_6_2x_17022022.jpg",
+}
+
+let sec8= {
+    img1: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_1__2x_17022022.jpg",
+    img2: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_2__2x_17022022.jpg",
+    img3: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_3__2x_17022022.jpg",
+    img4: "https://ii2.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_3__2x_17022022-1.jpg",
+    img5: "https://ii3.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_5__2x_17022022.jpg",
+    img6: "https://ii1.pepperfry.com/media/wysiwyg/banners/Homepage_Brands_WEB_6__2x_17022022.jpg",
+    img7: "https://ii2.pepperfry.com/media/wysiwyg/banners/mat-20-jan-1-n-1.jpg",
+    img8: "https://ii3.pepperfry.com/media/wysiwyg/banners/mod-20-jan-1-n-1.jpg",
+}
+
+let sec9 = "https://ii1.pepperfry.com/images/hp_more_from_store_bg.jpg";
+
+let sec10 ={
+    img1: "https://ii1.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_7.jpg",
+    img2: "https://ii3.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_8.jpg",
+    img3: "https://ii2.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_9.jpg",
+}
+
+let sec11 = {
+    img1: "",
+    img2: "",
+    img3: "",
+    img4: "",
+    img5: "",
+    img6: "",
+}
